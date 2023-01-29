097edc5df5c14b2f31103853050a1ca365f137dd
Joshua-Golden <59923113+Joshua-Golden@users.noreply.github.com>
2023-01-29 21:45:27 +0000

    Update config.py

--- auth-app/auth/config.py
+++ auth-app/auth/config.py
@@ -2,5 +2,5 @@ import os
 import sqlite3
 
 class Config:
-    SECRET_KEY = os.environ.get('a407e11ecc0f751741fb542feaf26708')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = "sqlite:///auth.db"
